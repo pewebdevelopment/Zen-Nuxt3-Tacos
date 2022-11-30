5e20b679e5e3ab9385a8d5f532d57b3ec334da47
pewebdevelopment <paathshala.webdevelopment@gmail.com>
2022-11-30 22:12:32 +0530

    Taco Component

---
+++ configs/tacosData.js
@@ -0,0 +1,51 @@
+const tacos = [
+  {
+    name: "al pastor",
+    aka: "tacos de adobada",
+    meat: "pork steak",
+    region: "Central Mexican region of Puebla",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Tacos-al-Pastor.jpg/800px-Tacos-al-Pastor.jpg",
+  },
+  {
+    name: "tacos de asador",
+    aka: "grill tacos",
+    meat: "sliced beef",
+    region: "Northern States of Mexico",
+    image:
+      "https://media.blogto.com/uploads/2016/11/30/20161106-2048-TacosElAsador7.jpg?h=2500&cmd=resize&quality=70&w=1400",
+  },
+  {
+    name: "tacos de pescado",
+    aka: "fish tacos",
+    meat: "fish",
+    region: "Baja California",
+    image:
+      "https://thecozyapron.com/wp-content/uploads/2018/03/baja-fish-tacos_thecozyapron_1.jpg",
+  },
+  {
+    name: "tacos de camarones",
+    aka: "shrimp tacos",
+    meat: "fried shrimp",
+    region: "Baja California",
+    image: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Tacos.jpg",
+  },
+  {
+    name: "tacos dorados",
+    aka: "golden tacos",
+    meat: "shredded chicken",
+    region: "",
+    image:
+      "https://www.maricruzavalos.com/wp-content/uploads/2020/07/rolled_taquitos_dorados_with_chicken.webp",
+  },
+  {
+    name: "tacos sudados",
+    aka: "sweaty tacos",
+    meat: "spicy lamb",
+    region: "central mexico",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Tacos_de_suadero.jpg/1280px-Tacos_de_suadero.jpg",
+  },
+];
+
+export default tacos;
---
+++ pages/myTacos.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Taco></Taco>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
